a307a21605bb905257e3cf045ab1830da090563b
victorokonkwo <victor4just@hotmail.com>
2024-02-14 12:44:22 +0100

    added stress test

--- app.py
+++ app.py
@@ -4,33 +4,39 @@ import numpy as np
 from prediction import predict
 
 
-st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into \
-     (setosa, versicolor, virginica) based on their sepal/petal \
-    and length/width.')
+st.title('Capital Adequecy Model')
+st.markdown('CAR model prediction for stress testing.')
 
-st.header("Plant Features")
-col1, col2 = st.columns(2)
+st.header("Stress Features")
+
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+    st.text("non-interest characteristics")
+    total_noninterest_expense_reported = st.slider('total noninterest expense reported', 0, 1000000000, 1000000)
+    total_noninterest_income_reported = st.slider('total noninterest income reported', 0, 1000000000,1000000)
 
 with col2:
-    st.text("Pepal characteristics")
-    petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+    st.text("interest characteristics")
+    total_interest_expense_reported = st.slider('total interest expense reported', 0, 1000000000, 1000000)
+    total_interest_income_reported = st.slider('total interest income reported', 0, 1000000000, 1000000)
+
+with col3:
+    st.text("loss characteristics")
+    provision_losses = st.slider('provision.losses', 0, 1000000000, 1000000)
+  
 
 st.text('')
-if st.button("Predict type of Iris"):
+if st.button("Predict Capital Adequecy Ratio (CAR)"):
     result = predict(
-        np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
-    st.text(result[0])
-
+        np.array([[total_noninterest_expense_reported, 
+        total_noninterest_income_reported, 
+        total_interest_expense_reported, 
+        total_interest_income_reported,
+        provision_losses
+        ]]))
+    
+    st.text(f'The capital adequecy ratio is {round(result[0], 3)}%')
 
 st.text('')
 st.text('')
-st.markdown(
-    '`Create by` [santiviquez](https://twitter.com/santiviquez) | \
-         `Code:` [GitHub](https://github.com/santiviquez/iris-streamlit)')
--- model.py
+++ model.py
@@ -1,37 +1,81 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+import pandas as pd 
 import joblib
 
 # random seed
 seed = 42
 
-# Read original dataset
-iris_df = pd.read_csv("data/iris.csv")
-iris_df.sample(frac=1, random_state=seed)
+nRowsRead = 1000 # specify 'None' if want to read whole file
+df1 = pd.read_csv('data/SupervisorySeverelyAdverseDomestic.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'SupervisorySeverelyAdverseDomestic.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
 
-# selecting features and target data
-X = iris_df[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
-y = iris_df[['Species']]
+income_hist = pd.read_excel('data/Income_Hist_Data.xlsx')
+income_hist['T1CR'] = (income_hist['tier.1.capital.reported'] / income_hist['risk.weighted.assets.reported']) * 100
+income_hist = income_hist.rename(columns={'Unnamed: 0': 'timeline'})
 
-# split data into train and test sets
-# 70% training and 30% test
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=seed, stratify=y)
+income_hist['year'] = income_hist['timeline'].apply(lambda x: x[:4])
+bhc_all = income_hist[['year', 'T1CR']].groupby('year').mean().reset_index().sort_values(by='year')
 
-# create an instance of the random forest classifier
-clf = RandomForestClassifier(n_estimators=100)
+stressed_bhc = income_hist.dropna()
+stressed_bhc = stressed_bhc[['year', 'T1CR']].groupby('year').mean().reset_index().sort_values(by='year')
 
-# train the classifier on the training data
-clf.fit(X_train, y_train)
+Y9C_bank = pd.read_csv('data/Y9C_banks_all_data_v13.csv')
+# bank metric
 
-# predict on the test set
-y_pred = clf.predict(X_test)
+Y9C_bank['NIC'] = (Y9C_bank['bhck4074'] / Y9C_bank['bhck3368']) * 400
+Y9C_bank['NIIC'] = (Y9C_bank['bhck4079'] / Y9C_bank['bhck3368']) * 400
+Y9C_bank['NIE'] = (Y9C_bank['bhck4093'] / Y9C_bank['bhck3368']) * 400
+Y9C_bank['PPNR'] = ((Y9C_bank['NIC']  + Y9C_bank['NIIC'] - Y9C_bank['NIE']) / Y9C_bank['bhck3368']) * 400
+Y9C_bank['NCO'] = ((Y9C_bank['bhck4635'] - Y9C_bank['bhck4605']) / Y9C_bank['bhck3368']) * 400
+Y9C_bank['commercial_loan'] = ((Y9C_bank['bhck1763'] + Y9C_bank['bhck1764']) / Y9C_bank['bhck3368']) * 100
+Y9C_bank['real_estate_loan'] = (Y9C_bank['bhck1410'] / Y9C_bank['bhck3368']) * 100
+Y9C_bank['size'] = Y9C_bank['bhck2170'].apply(lambda x: np.log(x)) 
 
-# calculate accuracy
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")  # Accuracy: 0.91
+income_hist['rssd9001'] = income_hist['institution.name'].map({
+                                    'Citigroup Inc.': 1951350,
+                                    'JPMorgan Chase & Co.': 1039502
+                                    })
+income_hist_df = income_hist.merge(Y9C_bank, on='rssd9001')
+
+supervisory_df = pd.read_csv('data/SupervisorySeverelyAdverseDomestic.csv')
+supervisory_df['Quarter'] = supervisory_df['Date'].apply(lambda x: x.split(' ')[0])
+supervisory_df['Year'] = supervisory_df['Date'].apply(lambda x: x.split(' ')[1])
+supervisory_df['timeline'] = supervisory_df['Year'] + supervisory_df['Quarter']
+
+total_df = income_hist_df.merge(supervisory_df, on='timeline')
+total_df['year'] = total_df['timeline'].apply(lambda x: x[:4])
+risk_df = total_df[['year', 'risk.weighted.assets.reported', 'institution.name']].dropna().groupby([
+                                                                'year', 'institution.name']).mean().reset_index()
+risk_jpmorgan = risk_df[risk_df['institution.name'] == 'JPMorgan Chase & Co.']
+risk_df_citi = risk_df[risk_df['institution.name'] == 'Citigroup Inc.']
+
+pred_col = ['total.interes.income.reported',
+ 'total.interest.expense.reported',
+ 'total.noninterest.income.reported',
+ 'total.noninterest.expense.reported',
+ 'provision.losses',
+ 'T1CR',
+'year',
+'institution.name'         
+]
+
+total_data = total_df[pred_col].drop_duplicates().dropna()
+X = total_data.drop(['T1CR', 'year', 'institution.name'], axis=1)
+y = total_data['T1CR']
+
+# x_test = total_data[total_data['institution.name'] == 'JPMorgan Chase & Co.'].drop(['T1CR', 'year', 'institution.name'], axis=1)
+# y_test = total_data[total_data['institution.name'] == 'JPMorgan Chase & Co.']['T1CR']
+
+rf = RandomForestRegressor()
+rf.fit(X, y)
 
 # save the model to disk
-joblib.dump(clf, "rf_model.sav")
+joblib.dump(rf, "rf_model.sav")
