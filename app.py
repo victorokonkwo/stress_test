e7b043d559e7f9c532c8cbfed19e717e48290d4d
victorokonkwo <victor4just@hotmail.com>
2024-02-14 14:20:32 +0100

    change title

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from prediction import predict
 
 
-st.title('Capital Adequecy Model')
+st.title('Capital Adequacy Model')
 st.markdown('CAR model prediction for stress testing.')
 
 st.header("Stress Features")
@@ -12,18 +12,18 @@ st.header("Stress Features")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.text("non-interest characteristics")
+    st.text("Non-interest characteristics")
     total_noninterest_expense_reported = st.slider('total noninterest expense reported', 0, 1000000000, 1000000)
     total_noninterest_income_reported = st.slider('total noninterest income reported', 0, 1000000000,1000000)
 
 with col2:
-    st.text("interest characteristics")
+    st.text("Interest characteristics")
     total_interest_expense_reported = st.slider('total interest expense reported', 0, 1000000000, 1000000)
     total_interest_income_reported = st.slider('total interest income reported', 0, 1000000000, 1000000)
 
 with col3:
-    st.text("loss characteristics")
-    provision_losses = st.slider('provision.losses', 0, 1000000000, 1000000)
+    st.text("Loss characteristics")
+    provision_losses = st.slider('provision losses', 0, 1000000000, 1000000)
   
 
 st.text('')
@@ -36,7 +36,7 @@ if st.button("Predict Capital Adequecy Ratio (CAR)"):
         provision_losses
         ]]))
     
-    st.text(f'The capital adequecy ratio is {round(result[0], 3)}%')
+    st.text(f'The Capital Adequacy ratio is {round(result[0], 3)}%')
 
 st.text('')
 st.text('')
