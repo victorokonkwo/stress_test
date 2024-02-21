4b6f9b28b67800e64343b6e0c9f7c4530be0af16
victorokonkwo <victor4just@hotmail.com>
2024-02-22 00:33:36 +0100

    added value

--- app.py
+++ app.py
@@ -13,11 +13,20 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.text("RWA characteristics")
-    credit_rwa = st.slider('CREDIT RWA', 0, 100000000000, 100000)
-    operational_rwa = st.slider('OPERATIONAL RWA', 0, 10000000000,100000)
-    market_rwa = st.slider('MARKET RWA', 0, 10000000000,100000)
-    rwa = st.slider('RWA', 0, 10000000000,100000)
-    capital = st.slider('capital', 0, 10000000000,100000)
+    # credit_rwa = st.slider('CREDIT RWA', 0, 100000000000, 100000)
+    # operational_rwa = st.slider('OPERATIONAL RWA', 0, 10000000000,100000)
+    # market_rwa = st.slider('MARKET RWA', 0, 10000000000,100000)
+    # rwa = st.slider('RWA', 0, 10000000000,100000)
+    # capital = st.slider('capital', 0, 10000000000,100000)
+    credit_rwa = st.text_input('CREDIT RWA', value='0')
+    operational_rwa = st.text_input('OPERATIONAL RWA', value='0')
+    market_rwa = st.text_input('MARKET RWA', value='0')
+    rwa = st.text_input('RWA', value='0')
+    capital = st.text_input('capital', value='0')
+
+
+
+
 
 st.text('')
 st.text('')
@@ -25,23 +34,27 @@ st.text('')
 st.text('')
 
 with col2:
-    st.text("capital tier characteristics")
-    capital_tier2 = st.slider('Capital Tier2', 0, 100000000000, 100000)
-    total_qualifying_capital = st.slider('Total Qualifying Capital', 0, 100000000000, 100000)
-    tier1_to_twra = st.slider('TIER 1 TO TWRA (%)', 0, 100000000000, 100000)
-    tier2_to_twra = st.slider('TIER 2 TO TWRA (%)', 0, 100000000000, 100000)
+    st.text("Capital Tier Characteristics")
+    # capital_tier2 = st.slider('Capital Tier2', 0, 100000000000, 100000)
+    # total_qualifying_capital = st.slider('Total Qualifying Capital', 0, 100000000000, 100000)
+    # tier1_to_twra = st.slider('TIER 1 TO TWRA (%)', 0, 100000000000, 100000)
+    # tier2_to_twra = st.slider('TIER 2 TO TWRA (%)', 0, 100000000000, 100000)
+    capital_tier2 = st.text_input('Capital Tier2', value='0')
+    total_qualifying_capital = st.text_input('Total Qualifying Capital', value='0')
+    tier1_to_twra = st.text_input('TIER 1 TO TWRA (%)', value='0')
+    tier2_to_twra = st.text_input('TIER 2 TO TWRA (%)', value='0')
 
 if st.button("Predict Capital Adequacy Ratio (CAR)"):
     result = predict(
-        np.array([[credit_rwa, 
-        operational_rwa, 
-        market_rwa, 
-        rwa,
-        capital,
-        capital_tier2,
-        total_qualifying_capital,
-        tier1_to_twra,
-        tier2_to_twra
+        np.array([[float(credit_rwa), 
+        float(operational_rwa), 
+        float(market_rwa), 
+        float(rwa),
+        float(capital),
+        float(capital_tier2),
+        float(total_qualifying_capital),
+        float(tier1_to_twra),
+        float(tier2_to_twra)
         ]]))
     
     st.text(f'The Capital Adequacy ratio is {round(result[0], 3)}%')
