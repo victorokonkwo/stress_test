89d39367ae2516fbe67e771241adb42e1bbf424d
victorokonkwo <victor4just@hotmail.com>
2024-02-22 09:23:35 +0100

    renamed title

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.text("RWA characteristics")
-    credit_rwa = st.text_input('CREDIT RWA', value='0')
-    operational_rwa = st.text_input('OPERATIONAL RWA', value='0')
-    market_rwa = st.text_input('MARKET RWA', value='0')
-    rwa = st.text_input('RWA', value='0')
-    capital = st.text_input('capital tier1', value='0')
+    credit_rwa = st.text_input('CREDIT RWA', value='')
+    operational_rwa = st.text_input('OPERATIONAL RWA', value='')
+    market_rwa = st.text_input('MARKET RWA', value='')
+    rwa = st.text_input('TRWA', value='')
+    capital = st.text_input('Capital Tier1', value='')
 
 
 
@@ -30,10 +30,10 @@ st.text('')
 
 with col2:
     st.text("Capital Tier Characteristics")
-    capital_tier2 = st.text_input('Capital Tier2', value='0')
-    total_qualifying_capital = st.text_input('Total Qualifying Capital', value='0')
-    tier1_to_twra = st.text_input('TIER 1 TO TWRA (%)', value='0')
-    tier2_to_twra = st.text_input('TIER 2 TO TWRA (%)', value='0')
+    capital_tier2 = st.text_input('Capital Tier2', value='')
+    total_qualifying_capital = st.text_input('Total Qualifying Capital', value='')
+    tier1_to_twra = st.text_input('TIER 1 TO TWRA (%)', value='')
+    tier2_to_twra = st.text_input('TIER 2 TO TWRA (%)', value='')
 
 if st.button("Predict Capital Adequacy Ratio (CAR)"):
     result = predict(
